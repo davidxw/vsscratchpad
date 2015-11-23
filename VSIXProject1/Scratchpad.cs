c657fe8dbc450fadd9d9582d2a31138fd674af81
davidxw <david.w@tson.net.au>
2015-11-23 12:30:33 +1100

    Upload existing code

---
+++ VSIXProject1/Scratchpad.cs
@@ -0,0 +1,70 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="Scratchpad.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace VSIXProject1
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Timers;
+    using Microsoft.VisualStudio.Shell;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("ddbe1f11-da6b-45a5-8598-037c5ba13893")]
+    public class Scratchpad : ToolWindowPane
+    {
+        private string fileSavePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Scratchpad"/> class.
+        /// </summary>
+        public Scratchpad() : base(null)
+        {
+            fileSavePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "vs_scratchpad.txt");
+
+            this.Caption = "Scratchpad";
+
+            this.Content = new ScratchpadControl();
+        }
+
+        protected override void OnCreate()
+        {
+            try
+            {
+                ((ScratchpadControl)this.Content).ScratchPadContent.Text = System.IO.File.ReadAllText(fileSavePath);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+        }
+
+        protected override void OnClose()
+        {
+            try
+            {
+                System.IO.File.WriteAllText(fileSavePath, ((ScratchpadControl)this.Content).ScratchPadContent.Text);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+        }
+
+        
+    }
+}
---
+++ VSIXProject1/ScratchpadControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ScratchpadControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace VSIXProject1
+{
+    using System;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Timers;
+    using System.Windows;
+    using System.Windows.Controls;
+
+
+
+    /// <summary>
+    /// Interaction logic for ScratchpadControl.
+    /// </summary>
+    public partial class ScratchpadControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScratchpadControl"/> class.
+        /// </summary>
+        public ScratchpadControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ScratchPadContent.Text = String.Empty;
+        }
+    }
+}
